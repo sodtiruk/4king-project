c5dbf111f8aa9efb3242d6622c8870a259ce6c3c
sodtiruk <sodtiruk22@gmail.com>
2024-03-12 19:37:23 +0700

    setup tailwind finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleCount = () => {
+    setCount( count + 1 );
+  }
 
   return (
     <>
-      
+      <h1 className='text-3xl underline font-bold'>
+        Hello world!
+      </h1>
     </>
   )
 }
